70b10c6587f271f85d41f586a5578ff4587423c2
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-06 15:53:49 -0300

    Update

--- app/src/main/java/com/example/petsapp/MainActivity.java
+++ app/src/main/java/com/example/petsapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.petsapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import com.example.petsapp.data.PetContract;
+import com.example.petsapp.data.PetDbHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -12,6 +16,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,9 +34,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,EditorActivity.class);
                 startActivity(intent);
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+        displayDatabaseInfo();
+    }
+
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        PetDbHelper mDbHelper = new PetDbHelper(this);
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + PetContract.PetsEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 
     @Override
--- app/src/main/java/com/example/petsapp/data/PetDbHelper.java
+++ app/src/main/java/com/example/petsapp/data/PetDbHelper.java
@@ -3,13 +3,14 @@ package com.example.petsapp.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class PetDbHelper extends SQLiteOpenHelper {
     /** Database Version and Name */
-    public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "shelter.db";
+    public static final int DATABASE_VERSION = 1;
     /** Constructor method */
 
     public PetDbHelper(@Nullable Context context) {
@@ -18,9 +19,15 @@ public class PetDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        /*String s = "CREATE TABLE " + PetContract.PetsEntry.TABLE_NAME + "("
-                + PetContract.PetsEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT"*/
-        //db.execSQL();
+        //CREATE TABLE pets (_id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,breed TEXT,gender INTEGER NOT NULL,weight INTEGER NOT NULL DEFAULT 0);
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetContract.PetsEntry.TABLE_NAME + "("
+                + PetContract.PetsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetContract.PetsEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetContract.PetsEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetContract.PetsEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetContract.PetsEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0"
+                + ");";
+        db.execSQL(SQL_CREATE_PETS_TABLE);
     }
 
     @Override
